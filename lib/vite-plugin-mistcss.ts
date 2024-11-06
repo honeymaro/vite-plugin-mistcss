9df3d3d746c74d345aa1e7bf7a3b7cec5e5dec00
maro <honeymaro@live.co.kr>
2024-11-06 17:21:47 +0900

    v1.0.0-beta.2

--- lib/vite-plugin-mistcss.ts
+++ lib/vite-plugin-mistcss.ts
@@ -1,106 +1,150 @@
 import { Plugin } from "vite";
-import { parse } from "mistcss/lib/parser";
-import { render as renderReact } from "mistcss/lib/renderers/react";
-import { existsSync, readFileSync, writeFileSync } from "fs";
+// @ts-expect-error mistcss has no type definition
+import mistcss from "mistcss";
+import { readFile, unlink } from "fs/promises";
 import path from "path";
-import { globby } from "globby";
-import { unlink } from "fs/promises";
+import postcss, { Processor } from "postcss";
+import { glob } from "tinyglobby";
+import { existsSync } from "fs";
 
-type RenderFunction = typeof renderReact;
-type Extension = ".tsx" | ".astro" | ".svelte";
-type Target = "react" | "hono" | "astro" | "vue" | "svelte";
-
-interface MistcssPluginOptions {
-  target?: Target;
-}
-
-function getFileExtension(target: Target): Extension {
-  if (target === "react" || target === "hono" || target === "vue") {
-    return ".tsx";
-  }
-  return `.${target}`;
-}
-
-async function getRenderFunction(target: Target) {
-  const rendererName = target === "hono" ? "react" : target;
-  const renderer = await import(`mistcss/lib/renderers/${rendererName}.js`);
-
-  const render = renderer.render as RenderFunction;
-
-  if (target === "hono") {
-    return (...args: Parameters<RenderFunction>) =>
-      render(args[0], args[1], true);
-  }
-
-  return render;
-}
-
-function createFile(
-  mistFilename: string,
-  renderFunction: RenderFunction,
-  outputExtension: Extension
-) {
-  try {
-    const data = parse(readFileSync(mistFilename, "utf8"));
-    const name = path.basename(mistFilename, ".mist.css");
-    if (data[0]) {
-      const result = renderFunction(name, data[0]);
-
-      writeFileSync(mistFilename.replace(/\.css$/, outputExtension), result);
-    }
-  } catch (e) {
-    if (e instanceof Error) {
-      console.error(`Error ${mistFilename}: ${e.message}`);
-    } else {
-      console.error(`Error ${mistFilename}`);
-      console.error(e);
-    }
-  }
-}
-
-async function removeFile(filename: string) {
+async function removeFileIfExists(filename: string) {
   await unlink(filename).catch(() => false);
 }
 
-export default function mistcssPlugin({
-  target = "react",
-}: MistcssPluginOptions = {}): Plugin {
-  let renderFunction: RenderFunction;
-  let outputExtension: Extension;
+const windowsSlashRE = /\\/g;
+function slash(p: string): string {
+  return p.replace(windowsSlashRE, "/");
+}
+
+const isMistCSSFilename = (id: string) => path.basename(id) === "mist.css";
+
+interface MistCSSPluginOptions {
+  cleanBeforeStart?: boolean;
+  keepUnused?: boolean;
+}
+
+export default function mistcssPlugin(options?: MistCSSPluginOptions): Plugin {
+  let processor: Processor | undefined = undefined;
+  let timerId: ReturnType<typeof setTimeout> | undefined = undefined;
+  const warmupList: string[] = [];
+
+  const runMistCSS = async (filename: string) => {
+    if (!processor) {
+      return;
+    }
+    try {
+      const code = await readFile(filename);
+      processor.process(code, { from: filename }).catch(console.log);
+    } catch {
+      // ignore
+    }
+  };
 
   return {
     name: "vite-plugin-mistcss",
+    configResolved(config) {
+      const resolve = (p: string) => path.resolve(config.root, p);
+      const options = config.build;
+      const libOptions = options.lib;
+      const input = libOptions
+        ? options.rollupOptions?.input ||
+          (typeof libOptions.entry === "string"
+            ? resolve(libOptions.entry)
+            : Array.isArray(libOptions.entry)
+            ? libOptions.entry.map(resolve)
+            : Object.fromEntries(
+                Object.entries(libOptions.entry).map(([alias, file]) => [
+                  alias,
+                  resolve(file),
+                ])
+              ))
+        : typeof options.ssr === "string"
+        ? resolve(options.ssr)
+        : options.rollupOptions?.input || resolve("index.html");
+      const inputs =
+        typeof input === "string"
+          ? [input]
+          : Array.isArray(input)
+          ? input
+          : Object.values(input);
 
-    async config() {
-      renderFunction = await getRenderFunction(target);
-      outputExtension = getFileExtension(target);
+      inputs.forEach((input) => {
+        warmupList.push(input);
+      });
+    },
+
+    configureServer(server) {
+      warmupList.forEach((filename) => {
+        if (filename.endsWith(".html")) {
+          readFile(filename, "utf-8").then((html) => {
+            server.transformIndexHtml(filename, html).catch(() => false);
+          });
+          return;
+        }
+        server.warmupRequest(filename).catch(() => false);
+      });
+      const serverWatchHandler = (filename: string) => {
+        if (filename.endsWith(".d.ts")) {
+          return;
+        }
+        if (server.moduleGraph.getModuleById(slash(filename))?.file) {
+          server.warmupRequest(filename).catch(() => false);
+        }
+        if (timerId) {
+          clearTimeout(timerId);
+        }
+        timerId = setTimeout(() => {
+          server.moduleGraph.idToModuleMap.forEach((moduleNode) => {
+            if (moduleNode.file && isMistCSSFilename(moduleNode.file)) {
+              if (moduleNode.importers.size === 0) {
+                if (!options?.keepUnused) {
+                  removeFileIfExists(
+                    moduleNode.file.replace(/\.css$/, ".d.ts")
+                  );
+                }
+                return;
+              }
+              runMistCSS(moduleNode.file);
+            }
+          });
+        }, 100);
+      };
+      server.watcher.on("change", serverWatchHandler);
+      server.watcher.on("add", serverWatchHandler);
+    },
+
+    load(id) {
+      if (isMistCSSFilename(id)) {
+        runMistCSS(id);
+      }
     },
 
     async buildStart() {
-      // Build out files
-      (await globby("**/*.mist.css")).forEach((mistFilename) =>
-        createFile(mistFilename, renderFunction, outputExtension)
-      );
-
-      // Clean out files without a matching mist file
-      (await globby(`**/*.mist${outputExtension}`)).forEach((componentFilename) => {
-        const mistFilename = componentFilename.replace(
-          new RegExp(`${outputExtension}$`),
-          ".css"
+      if (!mistcss.postcss) {
+        console.warn(
+          "The installed version of mistcss does not support. Please upgrade to mistcss@1.0.0 or later or use the previous version of this plugin."
         );
-        if (!existsSync(mistFilename)) {
-          removeFile(componentFilename);
+        return;
+      }
+      processor = postcss([mistcss()]);
+      const filenames = await glob("**/mist.d.ts", {
+        ignore: ["node_modules"],
+      });
+      filenames.forEach((filename) => {
+        if (
+          options?.cleanBeforeStart ||
+          (!options?.keepUnused &&
+            !existsSync(filename.replace(/\.d\.ts$/, ".css")))
+        ) {
+          removeFileIfExists(filename);
         }
       });
     },
 
     watchChange(filename, change) {
-      if (filename.endsWith(".mist.css")) {
-        if (change.event === "update" || change.event === "create") {
-          createFile(filename, renderFunction, outputExtension);
-        }
+      if (path.basename(filename) === "mist.css") {
         if (change.event === "delete") {
-          removeFile(filename.replace(/\.css$/, outputExtension));
+          removeFileIfExists(filename.replace(/\.css$/, ".d.ts"));
         }
       }
     },
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
+    rollupOptions: {
+      external: ["mistcss"],
+    },
     ssr: true,
 
     lib: {
